2c53a579a90d5d9ac5aceac616cfa77841288ca1
Neil Shah <neilmshah01@gmail.com>
2018-09-10 16:26:30 -0700

    added superhero collection view

---
+++ flixMovieViewer(iOS)/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  flixMovieViewer(iOS)
+//
+//  Created by Neil Shah on 9/10/18.
+//  Copyright © 2018 Neil Shah. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
